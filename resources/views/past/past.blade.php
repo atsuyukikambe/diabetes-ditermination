de644a4749ee40d95b79a8695683f5e3487a8de3
atsuyuki kambe <kanbe7767@gmail.com>
2020-11-22 02:52:00 +0000

    diabetes-ditermination

--- resources/views/past/past.blade.php
+++ resources/views/past/past.blade.php
@@ -7,22 +7,51 @@
             margin-top: 100px;
         }
         .list-unstyled{
-            width: 800px;
-            margin: 0 auto;
+            width: auto;
             text-align: center;
-            margin-top: 75px;
+            margin: 75px auto 20px auto;
+        }
+        p{
+            width: auto;
+            margin: auto;
+        }
+        .center{
+            margin-bottom: 20px;
+            font-size: 30px;
         }
     </style>
     <div class="text-center">最近の結果</div>
-    <div class="row center">
     @if (count($results ?? '')>0)
         @foreach ($results as $result)
         <ul class="list-unstyled">
+            <a class="text-left">{{ $result->created_at }}</a>
+            <div class="center">
+                @if (($result->hba1c)>=6.5)
+                    @if (($result->mbgl)>=126 && ($result->ogtt)>=200)
+                    糖尿病です
+                    @elseif (($result->mbgl)>=126 || ($result->ogtt)>=200)
+                    糖尿病です
+                    @else
+                    糖尿病の疑いがあります
+                    @endif
+                @else 
+                    @if (($result->mbgl)>=126 && ($result->ogtt)>=200)
+                    糖尿病の疑いがあります
+                    @elseif (($result->mbgl)>=126 || ($result->ogtt)>=200)
+                    糖尿病の疑いがあります
+                    @elseif (($result->mbgl)<110 && ($result->ogtt)<140)
+                    糖尿病ではありません
+                    @elseif (($result->mbgl)<110 || ($result->ogtt)<140)
+                    糖尿病ではありません
+                    @else
+                    糖尿病に近いです
+                    @endif
+                @endif
+            </div>
             <p>朝の空腹時血糖値：   {{ ($result->mbgl) }}mg/dL</p>
             <p>75gブドウ糖負荷試験：    {{ ($result->ogtt) }}mg/dL</p>
             <p>HbA1c：  {{ ($result->hba1c) }}%</p>                  
         </ul>
         @endforeach
-    </div>
     @endif
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,17 +9,15 @@
             <h3>
                 <p>糖尿病とは血液中の血糖値が常に高い状態が続く疾患です。糖尿病は１型糖尿病と２型糖尿病の二種類に分かれます。</p>
 　              <p>１型糖尿病はインスリンを出すβ細胞が障害されることによって、インスリンがほとんど出なくなり、その結果、頻尿や急激な体重減少などの症状が現れます。</p>
-　              <p>２型糖尿病は１型とは違って作られたインスリンが十分機能せず（インスリン抵抗性）、感染症のリスクが高くなり、手足の感覚が低下するなどの症状が現れます。日本人のほとんどがこの２型糖尿病にかかり、主な原因としては過食・過飲・運動不足・ストレスなどの生活習慣が原因です。</p>
+　              <p>２型糖尿病は１型とは違って作られたインスリンが十分機能せず（インスリン抵抗性）、感染症のリスクが高くなり、手足の感覚が低下するなどの症状が現れます。日本人の糖尿病患者のほとんどがこの２型糖尿病にかかり、主な原因としては過食・過飲・運動不足・ストレスなどの生活習慣が原因です。</p>
 　              <p>ここではあなたが糖尿病であるか、またはどれだけ糖尿病に近いかを簡易的に測定することが可能です。</p>
             </h3>
         </div>
-        <div class="container">
-            <div class="row">
-                @if (\Auth::check())
-                {!! link_to_route('ditermination.ditermination', '判定スタート', ['id' => Auth::id()], ['class' => 'btn-square-shadow']) !!}
-                @else
-                {!! link_to_route('signup.get', '判定スタート', [], ['class' => 'btn-square-shadow']) !!}
-                @endif
-            </div>
+        <div class="row">
+            @if (\Auth::check())
+            {!! link_to_route('ditermination.ditermination', '判定スタート', ['id' => Auth::id()], ['class' => 'btn-square-shadow']) !!}
+            @else
+            {!! link_to_route('signup.get', '判定スタート', [], ['class' => 'btn-square-shadow']) !!}
+            @endif
         </div>
 @endsection
